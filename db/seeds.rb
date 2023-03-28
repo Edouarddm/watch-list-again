3d9e15139c48cd124afed195df319b33ab46e6c2
Edouard de Montchenu edouarddemontchenu@gmail.com <edouarddemontchenu@gmail.com>
2023-03-28 14:00:12 +0200

    Lists MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+require 'json'
+require 'open-uri'
+
+Movie.destroy_all
+puts 'DB CLEAN'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +10,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+url = 'https://tmdb.lewagon.com/movie/popular'
+url_2 = 'https://tmdb.lewagon.com/movie/top_rated'
+movies = JSON.parse(URI.open(url).read)['results']
+movies.each do |movie|
+  poster_url_path = 'https://image.tmdb.org/t/p/original'
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "#{poster_url_path}#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+movies = JSON.parse(URI.open(url_2).read)['results']
+movies.each do |movie|
+  poster_url_path = 'https://image.tmdb.org/t/p/original'
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "#{poster_url_path}#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts 'DB READY'
